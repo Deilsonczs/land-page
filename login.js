19a14c8f70ce1a920884196dee9024e8c243e368
Deilson Araujo <141389988+Deilsonczs@users.noreply.github.com>
2024-06-09 19:51:34 -0300

    add login

--- login.js
+++ login.js
@@ -1,28 +1,11 @@
-function login() {
-    const username = document.getElementById('username').value.trim();
-    const password = document.getElementById('password').value.trim();
-    const errorMessage = document.getElementById('error-message');
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-    // Limpar a mensagem de erro inicialmente
-    errorMessage.textContent = '';
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
 
-    // Verificar se os campos estão vazios
-    if (username === '' || password === '') {
-        return; // Não fazer nada se os campos estiverem vazios
-    }
-
-    fetch('credentials.json')
-        .then(response => response.json())
-        .then(credentials => {
-            const user = credentials.find(user => user.username === username && user.password === password);
-            if (user) {
-                window.location.href = 'controle.html'; // Substitua pelo URL da próxima página
-            } else {
-                errorMessage.textContent = 'usuario ou senha incorretos';
-            }
-        })
-        .catch(error => {
-            console.error('Erro ao ler o arquivo de credenciais:', error);
-            errorMessage.textContent = 'Erro ao processar o login';
-        });
-}
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
