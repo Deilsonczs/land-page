6da31cb1f6df3dee41c75c80a455820a77f89e52
Deilson Araujo <141389988+Deilsonczs@users.noreply.github.com>
2024-06-11 11:53:52 -0300

    add eye no campo senha

--- login.js
+++ login.js
@@ -9,3 +9,22 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+const togglePasswordIcons = document.querySelectorAll(".toggle-password");
+
+togglePasswordIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        const input = icon.previousElementSibling;
+        if (input.type === "password") {
+            input.type = "text";
+            icon.classList.add("active");
+            icon.classList.remove("fa-eye");
+            icon.classList.add("fa-eye-slash");
+        } else {
+            input.type = "password";
+            icon.classList.remove("active");
+            icon.classList.remove("fa-eye-slash");
+            icon.classList.add("fa-eye");
+        }
+    });
+});
